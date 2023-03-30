6282630f850d334afa0a80fc8e0bf047c6da374d
Tim Nicholas <tsnicholas@bsu.edu>
2023-03-30 11:13:09 -0400

    Creates connection via config file instead.

--- database.py
+++ database.py
@@ -1,12 +1,26 @@
 import mysql.connector
+from configparser import ConfigParser
+
+def read_config_file(filename, section):
+    parser = ConfigParser()
+    parser.read(filename)
+    db = {}
+    if parser.has_section(section):
+        items = parser.items(section)
+        for item in items:
+            db[item[0]] = item[1]
+    else:
+        raise Exception(f'{section} not found in {filename}')
+    return db
 
 def create_connection():
-    return mysql.connector.connect(
-        username = 'tsnicholas',
-        password = 'Ptic43vr',
-        host = 'localhost',
-        database = 'falconAirlines'
-    )
+    db_config = read_config_file('database.ini', 'mysql')
+    print('Connecting to MySQL database...')
+    conn = mysql.connector.connect(**db_config)
+    if conn.is_connected():
+        return conn
+    else:
+        raise mysql.connector.Error
 
 if __name__ == '__main__':
     try:
